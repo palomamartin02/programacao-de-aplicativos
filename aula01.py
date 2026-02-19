a7928bf3b180ab9479659b5469109012095af432
Paloma Martin <palomamartin668@gmail.com>
2026-02-19 10:04:59 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 15 #variável inteira
+peso = 63.3 #variável float
+nome = "Paloma Martin de Sousa" # variável string
+sexo_feminino = True # variável bool
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
