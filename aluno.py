6e7f0ff27ca91a47c02e77fedcab8f91523e4a0c
palomamartin02 <palomamartin668@gmail.com>
2026-02-20 10:00:19 -0300

    aula dia 20/02

---
+++ aluno.py
@@ -0,0 +1,9 @@
+nome = input ("digite o seu nome: ")
+idade = int (input("digite sua idade: "))
+telefone = int (input ("digite seu telefone: "))
+cidade = input ("digite sua cidade: ")
+
+print ("ola", nome)
+print ("sua idade é", idade)
+print ("seu telefone é", telefone)
+print ("você mora em", cidade)
