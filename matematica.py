893f6ce9cc51e0bda59d2479f2e0f482d7006869
palomamartin02 <palomamartin668@gmail.com>
2026-02-20 09:43:43 -0300

    aluno.py

---
+++ matematica.py
@@ -0,0 +1,12 @@
+valor1 = int (input("digite o valor1"))
+valor2 = int (input("digite o valor2"))
+
+soma = valor1 + valor2 
+subtracao = valor1 - valor2
+divisao = valor1 / valor2
+multiplicacao = valor1 * valor2
+
+print ("a soma é:", soma)
+print ("a subtração é:", subtracao)
+print ("a divisão é:", divisao )
+print ("a multiplicação é:", multiplicacao)
